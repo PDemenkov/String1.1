fecfaf3999e4c9ab22889b3f57b1771cda7636c3
PDemenkov <guapnik@gmail.com>
2022-09-29 18:47:17 +0300

    Дз выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        String firstName = "Семён ";
+        String middleName = "Семёнович ";
+        String lastName = "Иванов ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());//Задание2
+
+        System.out.println(fullName.replace('ё', 'е')); // Задание 3
     }
 }
